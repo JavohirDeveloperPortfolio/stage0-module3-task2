941ac1f6e876e0fb1b979c1a9f219b62bd58a814
JavohirDeveloperPortfolio <haqberdiyev99@bk.ru>
2022-11-10 18:19:55 +0500

    next attempt!

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -4,11 +4,7 @@ public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
         int value = 1000;
-        int value2 = 3000;
+        int valueTwo = 3000;
         int variables = 5000;
-        variable = 2000;
-        variable = 4000;
-        variables = 7000;
-        value = 90000;
     }
 }
